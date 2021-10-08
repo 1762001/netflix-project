e02bbe2e7eff52b863b39102dc945926544e69c8
1762001 <73011343+1762001@users.noreply.github.com>
2021-10-08 16:10:00 +0530

    yes

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAOYjPK-uFrCrQaUV4MyZDH8Clyjfaqgd8",
+    authDomain: "netflix-project-d805f.firebaseapp.com",
+    projectId: "netflix-project-d805f",
+    storageBucket: "netflix-project-d805f.appspot.com",
+    messagingSenderId: "617238778338",
+    appId: "1:617238778338:web:d57103d90efbbde65269bb",
+    measurementId: "G-3ZXX16QD4C"
+  };
